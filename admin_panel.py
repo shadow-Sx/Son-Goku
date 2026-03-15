630d3f6ec8f0afd06a595dd47a9b356b7d81d459
shadow-Sx <shadowsoya8@gmail.com>
2026-03-15 14:11:10 +0300

    Update admin_panel.py

--- admin_panel.py
+++ admin_panel.py
@@ -1,28 +1,16 @@
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def admin_panel():
-    kb = InlineKeyboardMarkup()
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
 
-    kb.add(InlineKeyboardButton("❄ Birlamchi sozlamalar", callback_data="primary_settings"))
-    kb.add(
-        InlineKeyboardButton("📊 Statistika", callback_data="stats"),
-        InlineKeyboardButton("✉ Xabar Yuborish", callback_data="send_msg")
-    )
-    kb.add(InlineKeyboardButton("📬 Post tayyorlash", callback_data="make_post"))
-    kb.add(
-        InlineKeyboardButton("🎥 Animelar sozlash", callback_data="anime_settings"),
-        InlineKeyboardButton("💳 Hamyonlar", callback_data="wallets")
-    )
-    kb.add(InlineKeyboardButton("🔍 Foydalanuvchini boshqarish", callback_data="user_manage"))
-    kb.add(
-        InlineKeyboardButton("📢 Kanallar", callback_data="channels"),
-        InlineKeyboardButton("🎛 Tugmalar", callback_data="buttons")
-    )
-    kb.add(InlineKeyboardButton("📄 Matnlar", callback_data="texts"))
-    kb.add(
-        InlineKeyboardButton("📋 Adminlar", callback_data="admins"),
-        InlineKeyboardButton("🤖 Bot holati", callback_data="bot_status")
-    )
-    kb.add(InlineKeyboardButton("◀️ Orqaga", callback_data="back_admin"))
+    kb.add(KeyboardButton("❄ Birlamchi sozlamalar"))
+    kb.add(KeyboardButton("📊 Statistika"), KeyboardButton("✉ Xabar Yuborish"))
+    kb.add(KeyboardButton("📬 Post tayyorlash"))
+    kb.add(KeyboardButton("🎥 Animelar sozlash"), KeyboardButton("💳 Hamyonlar"))
+    kb.add(KeyboardButton("🔍 Foydalanuvchini boshqarish"))
+    kb.add(KeyboardButton("📢 Kanallar"), KeyboardButton("🎛 Tugmalar"))
+    kb.add(KeyboardButton("📄 Matnlar"))
+    kb.add(KeyboardButton("📋 Adminlar"), KeyboardButton("🤖 Bot holati"))
+    kb.add(KeyboardButton("◀️ Orqaga"))
 
     return kb
