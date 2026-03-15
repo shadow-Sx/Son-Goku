0f3507f8a79d1304defa3c2396a04d22881caf21
shadow-Sx <shadowsoya8@gmail.com>
2026-03-15 20:54:17 +0300

    Update admin_menu.py

--- admin_menu.py
+++ admin_menu.py
@@ -2,7 +2,44 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def admin_panel():
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(KeyboardButton("📊 Statistika"))
-    kb.add(KeyboardButton("✉ Xabar yuborish"))
-    kb.add(KeyboardButton("⬅️ Orqaga"))
+
+    # 1-qator
+    kb.add(
+        KeyboardButton("❄️ Birlamchi sozlamalar"),
+        KeyboardButton("📊 Statistika")
+    )
+
+    # 2-qator
+    kb.add(
+        KeyboardButton("✉️ Xabar Yuborish"),
+        KeyboardButton("📬 Post tayyorlash")
+    )
+
+    # 3-qator
+    kb.add(
+        KeyboardButton("🎥 Animelar sozlash"),
+        KeyboardButton("💳 Hamyonlar")
+    )
+
+    # 4-qator
+    kb.add(
+        KeyboardButton("🔍 Foydalanuvchini boshqarish"),
+        KeyboardButton("📢 Kanallar")
+    )
+
+    # 5-qator
+    kb.add(
+        KeyboardButton("🎛️ Tugmalar"),
+        KeyboardButton("📄 Matnlar")
+    )
+
+    # 6-qator
+    kb.add(
+        KeyboardButton("📋 Adminlar"),
+        KeyboardButton("🤖 Bot holati")
+    )
+
+    # Orqaga
+    kb.add(KeyboardButton("◀️ Orqaga"))
+
     return kb
