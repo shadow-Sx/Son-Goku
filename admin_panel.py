129bfa3d262d39c9b5d8062d713c8d0ded3f660c
shadow-Sx <shadowsoya8@gmail.com>
2026-03-15 20:41:19 +0300

    Delete admin_panel.py

--- admin_panel.py
+++
@@ -1,7 +0,0 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-
-def admin_panel():
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(KeyboardButton("📊 Statistika"))
-    kb.add(KeyboardButton("✉ Xabar yuborish"))
-    return kb
