0e01934e7096aa73c5984ffb00eb86deff828101
shadow-Sx <shadowsoya8@gmail.com>
2026-03-18 01:51:57 +0300

    Update menu.py

--- handlers/user_manage/menu.py
+++ handlers/user_manage/menu.py
@@ -1,1 +1,9 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def user_manage_menu():
+    kb = InlineKeyboardMarkup()
+    kb.row(InlineKeyboardButton("➕ VIP qo‘shish", callback_data="vip_add"))
+    kb.row(InlineKeyboardButton("🗑 VIP o‘chirish", callback_data="vip_delete"))
+    kb.row(InlineKeyboardButton("📋 VIP ro‘yxati", callback_data="vip_list"))
+    kb.row(InlineKeyboardButton("◀️ Orqaga", callback_data="admin_back"))
+    return kb
