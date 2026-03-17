83af0ceb9b75b36bbe42b93c1cc4251ba77b9d88
shadow-Sx <shadowsoya8@gmail.com>
2026-03-17 06:03:32 +0300

    Update admin_menu.py

--- admin_menu.py
+++ admin_menu.py
@@ -1,30 +1,33 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def admin_panel():
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-
-    # 1-QATOR
-    kb.row("❄️ Birlamchi sozlamalar")
-
-    # 2-QATOR
-    kb.row("📊 Statistika", "✉️ Xabar yuborish")
-
-    # 3-QATOR
-    kb.row("📬 Post tayyorlash")
-
-    # 4-QATOR
-    kb.row("🎥 Animelar sozlash", "💳 Hamyonlar")
-
-    # 5-QATOR
-    kb.row("🔍 Foydalanuvchini boshqarish")
-
-    # 6-QATOR
-    kb.row("📢 Kanallar", "🎛 Tugmalar", "📄 Matnlar")
-
-    # 7-QATOR
-    kb.row("📋 Adminlar", "🤖 Bot holati")
-
-    # 8-QATOR
-    kb.row("◀️ Orqaga")
-
+    kb = InlineKeyboardMarkup()
+    kb.row(
+        InlineKeyboardButton("❄️ Birlamchi sozlamalar", callback_data="settings")
+    )
+    kb.row(
+        InlineKeyboardButton("📊 Statistika", callback_data="stats"),
+        InlineKeyboardButton("✉️ Xabar yuborish", callback_data="broadcast")
+    )
+    kb.row(
+        InlineKeyboardButton("📬 Post tayyorlash", callback_data="post")
+    )
+    kb.row(
+        InlineKeyboardButton("🎥 Animelar sozlash", callback_data="anime_menu")
+    )
+    kb.row(
+        InlineKeyboardButton("💳 Hamyonlar", callback_data="wallets")
+    )
+    kb.row(
+        InlineKeyboardButton("🔍 Foydalanuvchini boshqarish", callback_data="users")
+    )
+    kb.row(
+        InlineKeyboardButton("📢 Kanallar", callback_data="channels"),
+        InlineKeyboardButton("🎛 Tugmalar", callback_data="buttons"),
+        InlineKeyboardButton("📄 Matnlar", callback_data="texts")
+    )
+    kb.row(
+        InlineKeyboardButton("📋 Adminlar", callback_data="admins"),
+        InlineKeyboardButton("🤖 Bot holati", callback_data="bot_status")
+    )
     return kb
