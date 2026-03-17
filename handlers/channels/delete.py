c21e8a2ffac52446377b0e17cdb9e71f3e5f631c
shadow-Sx <shadowsoya8@gmail.com>
2026-03-17 23:04:03 +0300

    Update delete.py

--- handlers/channels/delete.py
+++ handlers/channels/delete.py
@@ -1,1 +1,10 @@
+from loader import bot, db
 
+@bot.callback_query_handler(func=lambda c: c.data.startswith("del_"))
+def delete_channel(call):
+    username = call.data.replace("del_", "")
+
+    db.forced_channels.delete_one({"username": username})
+
+    bot.answer_callback_query(call.id, "🗑 Kanal o‘chirildi!", show_alert=True)
+    bot.delete_message(call.message.chat.id, call.message.message_id)
