02e65580a06bda86873b735d70ae0aa5c2f2e8ba
shadow-Sx <shadowsoya8@gmail.com>
2026-03-17 07:07:14 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,1 +1,12 @@
+import os
+import telebot
+from pymongo import MongoClient
 
+TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7797502113"))
+
+bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
+
+mongo = MongoClient(MONGO_URI)
+db = mongo["anime_bot"]
