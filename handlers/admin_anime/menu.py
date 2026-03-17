70b3d85e8e8eba3f8aba09e87d38c122bb93185a
shadow-Sx <shadowsoya8@gmail.com>
2026-03-17 05:29:51 +0300

    Update menu.py

--- handlers/admin_anime/menu.py
+++ handlers/admin_anime/menu.py
@@ -1,1 +1,17 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def anime_menu():
+    kb = InlineKeyboardMarkup()
+    kb.row(
+        InlineKeyboardButton("➕ Anime qo‘shish", callback_data="anime_add")
+    )
+    kb.row(
+        InlineKeyboardButton("🎞 Qism qo‘shish", callback_data="anime_add_episode")
+    )
+    kb.row(
+        InlineKeyboardButton("📚 Anime ro‘yxati", callback_data="anime_list")
+    )
+    kb.row(
+        InlineKeyboardButton("✏️ Tahrirlash", callback_data="anime_edit")
+    )
+    return kb
