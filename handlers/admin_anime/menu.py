d3372a77b0e8181b2ae64c6cd6ec8e1270c02987
shadow-Sx <shadowsoya8@gmail.com>
2026-03-17 06:06:13 +0300

    Update menu.py

--- handlers/admin_anime/menu.py
+++ handlers/admin_anime/menu.py
@@ -1,17 +1,23 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from main import bot, ADMIN_ID
 
 def anime_menu():
     kb = InlineKeyboardMarkup()
-    kb.row(
-        InlineKeyboardButton("➕ Anime qo‘shish", callback_data="anime_add")
-    )
-    kb.row(
-        InlineKeyboardButton("🎞 Qism qo‘shish", callback_data="anime_add_episode")
-    )
-    kb.row(
-        InlineKeyboardButton("📚 Anime ro‘yxati", callback_data="anime_list")
-    )
-    kb.row(
-        InlineKeyboardButton("✏️ Tahrirlash", callback_data="anime_edit")
-    )
+    kb.row(InlineKeyboardButton("➕ Anime qo‘shish", callback_data="anime_add"))
+    kb.row(InlineKeyboardButton("🎞 Qism qo‘shish", callback_data="anime_add_episode"))
+    kb.row(InlineKeyboardButton("📚 Anime ro‘yxati", callback_data="anime_list"))
+    kb.row(InlineKeyboardButton("✏️ Tahrirlash", callback_data="anime_edit"))
     return kb
+
+
+@bot.callback_query_handler(func=lambda c: c.data == "anime_menu")
+def open_anime_menu(call):
+    if call.from_user.id != ADMIN_ID:
+        return
+
+    bot.send_message(
+        call.message.chat.id,
+        "🎥 <b>Animelar sozlash bo‘limi</b>",
+        reply_markup=anime_menu()
+    )
+    bot.answer_callback_query(call.id)
