b9305688cd3ff46bdccca6f57ced76d29673b464
shadow-Sx <shadowsoya8@gmail.com>
2026-03-17 07:25:39 +0300

    Update menu.py

--- handlers/admin_anime/menu.py
+++ handlers/admin_anime/menu.py
@@ -1,5 +1,5 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from main import bot, ADMIN_ID
+from loader import bot, ADMIN_ID
 from handlers.admin_panel.menu import big_admin_menu
 
 def anime_menu():
@@ -8,18 +8,17 @@ def anime_menu():
     kb.row(InlineKeyboardButton("🎞 Qism qo‘shish", callback_data="anime_add_episode"))
     kb.row(InlineKeyboardButton("📚 Anime ro‘yxati", callback_data="anime_list"))
     kb.row(InlineKeyboardButton("✏️ Tahrirlash", callback_data="anime_edit"))
+    kb.row(InlineKeyboardButton("🗑 Anime o‘chirish", callback_data="anime_delete"))
     kb.row(InlineKeyboardButton("◀️ Orqaga", callback_data="anime_back"))
     return kb
 
 
-# 🎥 Animelar sozlash → anime menyusi
 @bot.callback_query_handler(func=lambda c: c.data == "anime_menu")
 def open_anime_menu(call):
     bot.send_message(call.message.chat.id, "🎥 <b>Animelar sozlash</b>", reply_markup=anime_menu())
     bot.answer_callback_query(call.id)
 
 
-# ◀️ Orqaga → katta admin panelga qaytish
 @bot.callback_query_handler(func=lambda c: c.data == "anime_back")
 def back_to_admin(call):
     bot.send_message(call.message.chat.id, "◀️ Orqaga qaytdingiz", reply_markup=big_admin_menu())
