8b1ff915601021ce203694c250537c03205ee9f0
shadow-Sx <shadowsoya8@gmail.com>
2026-03-17 23:02:57 +0300

    Update menu.py

--- handlers/channels/menu.py
+++ handlers/channels/menu.py
@@ -1,1 +1,8 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def channels_menu():
+    kb = InlineKeyboardMarkup()
+    kb.row(InlineKeyboardButton("➕ Kanal qo‘shish", callback_data="add_channel"))
+    kb.row(InlineKeyboardButton("📋 Kanallar ro‘yxati", callback_data="list_channels"))
+    kb.row(InlineKeyboardButton("◀️ Orqaga", callback_data="admin_back"))
+    return kb
