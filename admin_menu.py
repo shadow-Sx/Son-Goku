e55f0690709180d22f82505a5970fdf956f22c0c
shadow-Sx <shadowsoya8@gmail.com>
2026-03-17 06:28:18 +0300

    Update admin_menu.py

--- admin_menu.py
+++ admin_menu.py
@@ -1,33 +1,6 @@
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def admin_panel():
-    kb = InlineKeyboardMarkup()
-    kb.row(
-        InlineKeyboardButton("❄️ Birlamchi sozlamalar", callback_data="settings")
-    )
-    kb.row(
-        InlineKeyboardButton("📊 Statistika", callback_data="stats"),
-        InlineKeyboardButton("✉️ Xabar yuborish", callback_data="broadcast")
-    )
-    kb.row(
-        InlineKeyboardButton("📬 Post tayyorlash", callback_data="post")
-    )
-    kb.row(
-        InlineKeyboardButton("🎥 Animelar sozlash", callback_data="anime_menu")
-    )
-    kb.row(
-        InlineKeyboardButton("💳 Hamyonlar", callback_data="wallets")
-    )
-    kb.row(
-        InlineKeyboardButton("🔍 Foydalanuvchini boshqarish", callback_data="users")
-    )
-    kb.row(
-        InlineKeyboardButton("📢 Kanallar", callback_data="channels"),
-        InlineKeyboardButton("🎛 Tugmalar", callback_data="buttons"),
-        InlineKeyboardButton("📄 Matnlar", callback_data="texts")
-    )
-    kb.row(
-        InlineKeyboardButton("📋 Adminlar", callback_data="admins"),
-        InlineKeyboardButton("🤖 Bot holati", callback_data="bot_status")
-    )
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.row(KeyboardButton("🛠 Boshqarish"))
     return kb
