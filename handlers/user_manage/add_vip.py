6bee8430fa3ec1566c8e03314a296b3796944742
shadow-Sx <shadowsoya8@gmail.com>
2026-03-18 01:52:54 +0300

    Update add_vip.py

--- handlers/user_manage/add_vip.py
+++ handlers/user_manage/add_vip.py
@@ -1,1 +1,27 @@
+from loader import bot, db, ADMIN_ID
 
+waiting_vip = {}
+
+@bot.callback_query_handler(func=lambda c: c.data == "vip_add")
+def ask_vip(call):
+    if call.from_user.id != ADMIN_ID:
+        return
+
+    bot.send_message(call.message.chat.id, "➕ VIP qilish uchun foydalanuvchi ID sini yuboring:")
+    waiting_vip[call.from_user.id] = True
+    bot.answer_callback_query(call.id)
+
+
+@bot.message_handler(func=lambda m: m.from_user.id in waiting_vip)
+def save_vip(message):
+    uid = message.from_user.id
+    try:
+        user_id = int(message.text.strip())
+    except:
+        bot.send_message(message.chat.id, "❌ Foydalanuvchi ID noto‘g‘ri.")
+        return
+
+    db.vip_users.insert_one({"user_id": user_id})
+
+    bot.send_message(message.chat.id, f"✅ <b>{user_id}</b> VIP foydalanuvchi sifatida qo‘shildi!")
+    waiting_vip.pop(uid)
