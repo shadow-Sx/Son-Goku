6d3d3853a005274f96e89d9edf65a9eb2ad0a7ee
shadow-Sx <shadowsoya8@gmail.com>
2026-03-18 04:41:07 +0300

    Update add.py

--- handlers/channels/add.py
+++ handlers/channels/add.py
@@ -5,7 +5,7 @@ waiting_channel = {}
 
 @bot.callback_query_handler(func=lambda c: c.data == "add_channel")
 def ask_channel(call):
-    bot.send_message(call.message.chat.id, "📢 Kanal username yuboring (masalan: @AniGonUz)")
+    bot.send_message(call.message.chat.id, "📢 Kanal username yuboring (masalan: @mychannel)")
     waiting_channel[call.from_user.id] = True
     bot.answer_callback_query(call.id)
 
@@ -16,10 +16,20 @@ def save_channel(message):
 
     try:
         chat = bot.get_chat(username)
+
+        print("CHAT OBJECT:", chat)
+        print("CHAT ID:", chat.id)
+        print("CHAT TYPE:", chat.type)
+
+        if chat.type != "channel":
+            bot.send_message(message.chat.id, "❌ Bu kanal emas. Faqat kanal qo‘shing.")
+            return
+
         channel_id = chat.id
         title = chat.title
-    except:
-        bot.send_message(message.chat.id, "❌ Kanal topilmadi yoki bot admin emas.")
+
+    except Exception as e:
+        bot.send_message(message.chat.id, f"❌ Xatolik: {e}")
         return
 
     db.forced_channels.insert_one({
