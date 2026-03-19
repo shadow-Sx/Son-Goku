5993862e5904855c22b76d363083250569d3b1ba
shadow-Sx <shadowsoya8@gmail.com>
2026-03-19 09:41:46 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "7797502113"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7026418050"))
 
 bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
 
